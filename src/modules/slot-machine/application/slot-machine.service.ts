ec8162a52544530360a1cad8d17074d19c6ccfcb
AntonioFSN2 <antonio.af@gec.inatel.br>
2026-03-29 03:54:03 -0300

    feat: add update, deactivate and remove methods to SlotMachineService

--- src/modules/slot-machine/application/slot-machine.service.ts
+++ src/modules/slot-machine/application/slot-machine.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SlotMachine } from '../domain/slot-machine.entity';
 import { CreateSlotMachineDto } from '../domain/dto/create-slot-machine.dto';
+import { UpdateSlotMachineDto } from '../domain/dto/update-slot-machine.dto';
 
 @Injectable()
 export class SlotMachineService {
@@ -28,5 +29,44 @@ export class SlotMachineService {
       throw new NotFoundException(`SlotMachine with ID ${id} not found`);
     }
     return SlotMachine;
+  }
+
+  async update(id: number, dto: UpdateSlotMachineDto): Promise<SlotMachine> {
+    const SlotMachine = await this.findOne(id);
+
+    if (dto.Name !== undefined) {
+      SlotMachine.Name = dto.Name;
+    }
+    if (dto.Description !== undefined) {
+      SlotMachine.Description = dto.Description;
+    }
+    if (dto.MinimumSpinValue !== undefined) {
+      SlotMachine.MinimumSpinValue = dto.MinimumSpinValue;
+    }
+    if (dto.MinimumChipsRequired !== undefined) {
+      SlotMachine.MinimumChipsRequired = dto.MinimumChipsRequired;
+    }
+    if (dto.MinimumRerollValue !== undefined) {
+      SlotMachine.MinimumRerollValue = dto.MinimumRerollValue;
+    }
+    if (dto.MaxRerolls !== undefined) {
+      SlotMachine.MaxRerolls = dto.MaxRerolls;
+    }
+    if (dto.Active !== undefined) {
+      SlotMachine.Active = dto.Active;
+    }
+
+    return this.SlotMachineRepo.save(SlotMachine);
+  }
+
+  async deactivate(id: number): Promise<SlotMachine> {
+    const SlotMachine = await this.findOne(id);
+    SlotMachine.Active = false;
+    return this.SlotMachineRepo.save(SlotMachine);
+  }
+
+  async remove(id: number): Promise<void> {
+    const SlotMachine = await this.findOne(id);
+    await this.SlotMachineRepo.remove(SlotMachine);
   }
 }
