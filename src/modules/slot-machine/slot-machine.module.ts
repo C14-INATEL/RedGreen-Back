078c2049ae73cf0b429382695daebb368304ec22
AntonioFSN2 <antonio.af@gec.inatel.br>
2026-04-07 22:24:21 -0300

    feat: integrate SlotSession module components into SlotMachineModule

--- src/modules/slot-machine/slot-machine.module.ts
+++ src/modules/slot-machine/slot-machine.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SlotMachineService } from './application/slot-machine.service';
 import { SlotMachineController } from './presentation/slot-machine.controller';
 import { SlotMachine } from './domain/slot-machine.entity';
+import { SlotSessionService } from './sessions/application/slot-session.service';
+import { SlotSessionController } from './sessions/presentation/slot-session.controller';
+import { SlotSession } from './sessions/domain/slot-session.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SlotMachine])],
-  providers: [SlotMachineService],
-  controllers: [SlotMachineController],
-  exports: [SlotMachineService],
+  imports: [TypeOrmModule.forFeature([SlotMachine, SlotSession])],
+  providers: [SlotMachineService, SlotSessionService],
+  controllers: [SlotMachineController, SlotSessionController],
+  exports: [SlotMachineService, SlotSessionService],
 })
 export class SlotMachineModule {}
